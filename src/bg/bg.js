b1e9f7890634937d1fa58bd55edb37164c4fd51a
Itsick <any.site.n.site@gmail.com>
2018-04-17 17:26:56 +0300

    after first render

--- src/bg/bg.js
+++ src/bg/bg.js
@@ -22,45 +22,45 @@ let state = loadState;
 
 // need to maintain listeners, because all already opened tabs will not be boosted otherwise...
 // furthermore, all tabs with changed URLs will not be boosted...
-// const tabListeners = {};
+const tabListeners = {};
 
-// // set listener for each created/reloaded tab
-// // this is needed because tabId and website URL will not be available otherwise inside the listener
-// chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-// 	// sync state on each refresh/creation to have less syncing penalty
-// 	state = loadState();
+// set listener for each created/reloaded tab
+// this is needed because tabId and website URL will not be available otherwise inside the listener
+chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
+	// sync state on each refresh/creation to have less syncing penalty
+	state = loadState();
 
-// 	// if the page is reloaded and the URL is changed or it is a first installation of the extension...
-// 	// tab object is IMMUTABLE, surprise surprise!
-// 	// So, we need to add new listeners for a tab with a new URL...
-// 	if ((change.status === 'loading') && (change.url || !tabListeners[tabId])) {
-// 		if (tabListeners[tabId]) {
-// 			chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
-// 		}
+	// if the page is reloaded and the URL is changed or it is a first installation of the extension...
+	// tab object is IMMUTABLE, surprise surprise!
+	// So, we need to add new listeners for a tab with a new URL...
+	if ((change.status === 'loading') && (change.url || !tabListeners[tabId])) {
+		if (tabListeners[tabId]) {
+			chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
+		}
 
-// 		tabListeners[tabId] = checkUrl(tab);
-// 		return chrome.webRequest.onBeforeRequest.addListener(
-// 			checkUrl, {
-// 			urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*'],
-// 			tabId: tab.id
-// 		},
-// 			["blocking"]
-// 		);
-// 	}
-// });
+		tabListeners[tabId] = checkUrl(tab);
+		return chrome.webRequest.onBeforeRequest.addListener(
+			checkUrl, {
+			urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*'],
+			tabId: tab.id
+		},
+			["blocking"]
+		);
+	}
+});
 
-// // cleanup all those listeners
-// chrome.tabs.onRemoved.addListener(function(tabId, removeObj) {
-// 	chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
-// 	return tabListeners[tabId] = null;
-// });
+// cleanup all those listeners
+chrome.tabs.onRemoved.addListener(function(tabId, removeObj) {
+	chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
+	return tabListeners[tabId] = null;
+});
 
 
-// // First time impression
-// chrome.runtime.onInstalled.addListener(function(details) {
-// 	if (details.reason === 'install') {
-// 		return chrome.tabs.create({url: 'https://www.facebook.com/WebBoostExtension/app/135876083099764/'});
-// 	}});
+// First time impression
+chrome.runtime.onInstalled.addListener(function(details) {
+	if (details.reason === 'install') {
+		return chrome.tabs.create({url: 'https://www.facebook.com/WebBoostExtension/app/135876083099764/'});
+	}});
 chrome.webRequest.onBeforeRequest.addListener(
 	checkUrl, {
 	urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*']
--- src/new/bg.js
+++ src/new/bg.js
@@ -1,51 +1,46 @@
 "use strict";
-var hashCheck_1 = require("./checkers/hashCheck");
-var regCheck_1 = require("./checkers/regCheck");
+var hash_check_1 = require("./checkers/hash-check");
+var reg_check_1 = require("./checkers/reg-check");
 var msvpCheck_1 = require("./checkers/msvpCheck");
 var requestInterceptor_1 = require("./misc/requestInterceptor");
 var url_1 = require("./misc/url");
 var state_1 = require("./misc/state");
-var checkers = [hashCheck_1.check, regCheck_1.check, msvpCheck_1.check];
+var checkers = [hash_check_1.check, reg_check_1.regCheck, msvpCheck_1.check];
 // webpage-specific settings are stored in this object
 var state = state_1.load();
 // need to maintain listeners, because all already opened tabs will not be boosted otherwise...
 // furthermore, all tabs with changed URLs will not be boosted...
-// const tabListeners = {};
-// // set listener for each created/reloaded tab
-// // this is needed because tabId and website URL will not be available otherwise inside the listener
-// chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-// 	// sync state on each refresh/creation to have less syncing penalty
-// 	state = loadState();
-// 	// if the page is reloaded and the URL is changed or it is a first installation of the extension...
-// 	// tab object is IMMUTABLE, surprise surprise!
-// 	// So, we need to add new listeners for a tab with a new URL...
-// 	if ((change.status === 'loading') && (change.url || !tabListeners[tabId])) {
-// 		if (tabListeners[tabId]) {
-// 			chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
-// 		}
-// 		tabListeners[tabId] = checkUrl(tab);
-// 		return chrome.webRequest.onBeforeRequest.addListener(
-// 			checkUrl, {
-// 			urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*'],
-// 			tabId: tab.id
-// 		},
-// 			["blocking"]
-// 		);
-// 	}
-// });
-// // cleanup all those listeners
-// chrome.tabs.onRemoved.addListener(function(tabId, removeObj) {
-// 	chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
-// 	return tabListeners[tabId] = null;
-// });
-// // First time impression
-// chrome.runtime.onInstalled.addListener(function(details) {
-// 	if (details.reason === 'install') {
-// 		return chrome.tabs.create({url: 'https://www.facebook.com/WebBoostExtension/app/135876083099764/'});
-// 	}});
-chrome.webRequest.onBeforeRequest.addListener(checkUrl, {
-    urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*']
-}, ["blocking"]);
+var tabListeners = {};
+// set listener for each created/reloaded tab
+// this is needed because tabId and website URL will not be available otherwise inside the listener
+chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
+    // sync state on each refresh/creation to have less syncing penalty
+    state = state_1.load();
+    // if the page is reloaded and the URL is changed or it is a first installation of the extension...
+    // tab object is IMMUTABLE, surprise surprise!
+    // So, we need to add new listeners for a tab with a new URL...
+    if ((change.status === 'loading') && (change.url || !tabListeners[tabId])) {
+        if (tabListeners[tabId]) {
+            chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
+        }
+        tabListeners[tabId] = checkUrl(tab);
+        return chrome.webRequest.onBeforeRequest.addListener(checkUrl, {
+            urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*'],
+            tabId: tab.id
+        }, ["blocking"]);
+    }
+});
+// cleanup all those listeners
+chrome.tabs.onRemoved.addListener(function (tabId, removeObj) {
+    chrome.webRequest.onBeforeRequest.removeListener(tabListeners[tabId]);
+    return tabListeners[tabId] = null;
+});
+// First time impression
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === 'install') {
+        return chrome.tabs.create({ url: 'https://www.facebook.com/WebBoostExtension/app/135876083099764/' });
+    }
+});
 var checkUrl = function (request) {
     if (request.method !== 'GET') {
         return requestInterceptor_1.ALLOW_REQUEST_TOKEN;
@@ -68,3 +63,6 @@ var checkUrl = function (request) {
 function __guard__(value, transform) {
     return (typeof value !== 'undefined' && value !== null) ? transform(value) : undefined;
 }
+chrome.webRequest.onBeforeRequest.addListener(checkUrl, {
+    urls: ['http://*/*', 'https://*/*', 'chrome-extension://*/*']
+}, ["blocking"]);
